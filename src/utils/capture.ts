17e6d107b32d450537c8c76c1beb031c3cf40207
s-yu-yu-yu <carefreektkt@gmail.com>
2021-01-26 03:26:49 +0900

    add: heroku用config

--- src/utils/capture.ts
+++ src/utils/capture.ts
@@ -5,11 +5,11 @@ export type UrlResponse = {
 }
 
 export const captureBanner = async (url: string): Promise<UrlResponse> => {
-  return await axios.get(`${process.env.VUE_APP_API_BASEURL}/banner?url=${url}`)
+  return await axios.get(`${process.env.VUE_APP_API_BASEURL}/banner/index?url=${url}`)
     .then(res => res.data)
 }
 
 export const captureThumbnail = async (url: string): Promise<UrlResponse> => {
-  return await axios.get(`${process.env.VUE_APP_API_BASEURL}/thumbnail?url=${url}`)
+  return await axios.get(`${process.env.VUE_APP_API_BASEURL}/thumbnail/index?url=${url}`)
     .then(res => res.data)
 }
